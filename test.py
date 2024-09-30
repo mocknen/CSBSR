e58c01ba22353add42db1cc98181debd1062bce1
Shogo Yamazaki <pgp@mocknen.net>
2024-09-30 10:47:52 +0900

    Enhance style consistency

--- test.py
+++ test.py
@@ -1,16 +1,14 @@
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## Toyota Technological Institute
-## Author: Yuki Kondo
-## Copyright (c) 2024
-## yuki.kondo.ab@gmail.com
-##
-## This source code is licensed under the Apache License license found in the
-## LICENSE file in the root directory of this source tree
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Toyota Technological Institute
+# Author: Yuki Kondo
+# Copyright (c) 2024
+# yuki.kondo.ab@gmail.com
+#
+# This source code is licensed under the Apache License license found in the
+# LICENSE file in the root directory of this source tree
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import argparse
-import datetime
-import os
 import re
 
 import torch
@@ -25,7 +23,8 @@ from model.data.retinal_dataset import RetinalDataSetTest
 from model.engine.inference import inference_for_ss, inference_tti_building
 from model.utils.misc import fix_model_state_dict
 from model.data.transforms.transforms import FactorResize
-from torch.multiprocessing import Pool, Process, set_start_method
+from torch.multiprocessing import set_start_method
+
 
 def test(args, cfg):
     device = torch.device(cfg.DEVICE)
@@ -42,37 +41,52 @@ def test(args, cfg):
             # print(f'------------Model Architecture-------------\n\n<Network SR>\n{model.sr_model}\n\n<Network SS>\n{model.segmentation_model}')
             pass
 
-    model.load_state_dict(fix_model_state_dict(torch.load(args.trained_model, map_location=lambda storage, loc:storage)))
+    model.load_state_dict(fix_model_state_dict(torch.load(
+        args.trained_model, map_location=lambda storage, loc: storage)))
     if 'indOptim' in cfg.OUTPUT_DIR:
         if cfg.MODEL.SR == 'KBPN':
             path = 'weights/KBPN_30000iter.pth'
         elif cfg.MODEL.SR == 'DBPN':
             path = 'weights/DBPN_30000iter.pth'
         print(f'load {path}')
-        m_key, u_key = model.load_state_dict(fix_model_state_dict(torch.load(path)), strict=False)
+        m_key, u_key = model.load_state_dict(
+            fix_model_state_dict(torch.load(path)), strict=False)
         assert len(u_key) == 0, (f'unexpected_keys are exist.\n {u_key}')
     model.eval()
 
     print('Loading Datasets...')
     test_transforms = TestTransforms(cfg)
-    sr_transforms = FactorResize(cfg.MODEL.SCALE_FACTOR, cfg.SOLVER.DOWNSCALE_INTERPOLATION)
+    sr_transforms = FactorResize(cfg.MODEL.SCALE_FACTOR,
+                                 cfg.SOLVER.DOWNSCALE_INTERPOLATION)
     if args.tti_crack_dataset:
-        test_dataset = TTICrackDataSetTest(cfg, cfg.DATASET.TEST_IMAGE_DIR, args.batch_size, transforms=test_transforms, )
+        test_dataset = TTICrackDataSetTest(cfg, cfg.DATASET.TEST_IMAGE_DIR,
+                                           args.batch_size,
+                                           transforms=test_transforms, )
     elif 'RetinalSeg' in cfg.DATASET.TEST_IMAGE_DIR:
-        test_dataset = RetinalDataSetTest(cfg, cfg.DATASET.TEST_IMAGE_DIR, cfg.DATASET.TEST_MASK_DIR,
-                                        cfg.DATASET.TEST_BLURED_DIR, cfg.DATASET.TEST_BLURED_NAME, args.batch_size,
-                                        transforms=test_transforms, sr_transforms=sr_transforms)
+        test_dataset = RetinalDataSetTest(cfg, cfg.DATASET.TEST_IMAGE_DIR,
+                                          cfg.DATASET.TEST_MASK_DIR,
+                                          cfg.DATASET.TEST_BLURED_DIR,
+                                          cfg.DATASET.TEST_BLURED_NAME,
+                                          args.batch_size,
+                                          transforms=test_transforms,
+                                          sr_transforms=sr_transforms)
     else:
-        test_dataset = CrackDataSetTest(cfg, cfg.DATASET.TEST_IMAGE_DIR, cfg.DATASET.TEST_MASK_DIR,
-                                        cfg.DATASET.TEST_BLURED_DIR, cfg.DATASET.TEST_BLURED_NAME, args.batch_size,
-                                        transforms=test_transforms, sr_transforms=sr_transforms)
+        test_dataset = CrackDataSetTest(cfg, cfg.DATASET.TEST_IMAGE_DIR,
+                                        cfg.DATASET.TEST_MASK_DIR,
+                                        cfg.DATASET.TEST_BLURED_DIR,
+                                        cfg.DATASET.TEST_BLURED_NAME,
+                                        args.batch_size,
+                                        transforms=test_transforms,
+                                        sr_transforms=sr_transforms)
     sampler = SequentialSampler(test_dataset)
-    batch_sampler = BatchSampler(sampler=sampler, batch_size=args.batch_size, drop_last=False)
-    test_loader = DataLoader(test_dataset, num_workers=args.num_workers, batch_sampler=batch_sampler)
+    batch_sampler = BatchSampler(sampler=sampler, batch_size=args.batch_size,
+                                 drop_last=False)
+    test_loader = DataLoader(test_dataset, num_workers=args.num_workers,
+                             batch_sampler=batch_sampler)
 
     if args.num_gpus > 1:
         device_ids = list(range(args.num_gpus))
-        print("device_ids:",device_ids)
+        print("device_ids:", device_ids)
         model = torch.nn.DataParallel(model, device_ids=device_ids)
     with torch.no_grad():
         if args.tti_crack_dataset:
@@ -80,8 +94,10 @@ def test(args, cfg):
         else:
             inference_for_ss(args, cfg, model, test_loader)
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Crack Segmentation with Blind Super Resolution(CSBSR)')
+    parser = argparse.ArgumentParser(
+        description='Crack Segmentation with Blind Super Resolution(CSBSR)')
     parser.add_argument('test_dir', type=str, default=None)
     parser.add_argument('iter_or_weight_name', type=str, default=None)
 
@@ -94,7 +110,8 @@ def main():
     parser.add_argument('--test_aiu', type=bool, default=True)
     parser.add_argument('--test_surface_distance', action="store_true")
     parser.add_argument('--test_classification_metrics', action="store_true")
-    parser.add_argument('--sf_save_image', action="store_false", help="If you do not want the output images to be saved, you should turn off this flag.")
+    parser.add_argument('--sf_save_image', action="store_false",
+                        help="If you do not want the output images to be saved, you should turn off this flag.")
     parser.add_argument('--origin_img_size', type=bool, default=True)
     parser.add_argument('--tti_crack_dataset', type=bool, default=False)
     parser.add_argument('--trained_model', type=str, default=None)
@@ -111,18 +128,18 @@ def main():
         _out_dir = f"iter_{args.iter_or_weight_name}"
         model_fname = f"iteration_{args.iter_or_weight_name}"
 
-    check_args = [('config_file', f'{args.test_dir}/config.yaml'),
-     ('output_dirname', f'{args.test_dir}/eval_AIU/{_out_dir}'),
-     ('trained_model', f'{args.test_dir}/model/{model_fname}.pth'),
+    check_args = [
+        ('config_file', f'{args.test_dir}/config.yaml'),
+        ('output_dirname', f'{args.test_dir}/eval_AIU/{_out_dir}'),
+        ('trained_model', f'{args.test_dir}/model/{model_fname}.pth'),
     ]
 
     if args.origin_img_size:
-        img_size = cfg.INPUT.IMAGE_SIZE # keep default
-
+        img_size = cfg.INPUT.IMAGE_SIZE  # keep default
 
     for check_arg in check_args:
         arg_name = f'args.{check_arg[0]}'
-        if exec(arg_name) == None:
+        if not exec(arg_name):
             exec(f'{arg_name} = "{check_arg[1]}"')
 
     cuda = torch.cuda.is_available()
@@ -133,14 +150,14 @@ def main():
         print('Configration file is loaded from {}'.format(args.config_file))
         cfg.merge_from_file(args.config_file)
 
-    if args.test_blured_name != None:
+    if args.test_blured_name:
         cfg.DATASET.TEST_BLURED_NAME = args.test_blured_name
         args.output_dirname = f'{args.test_dir}/eval_AIU/compe_blur/{_out_dir}_{args.test_blured_name}'
 
     if args.tti_crack_dataset:
         args.output_dirname = f'{args.test_dir}/eval_AIU/tti_bulinding/{_out_dir}_size64'
         cfg.DATASET.TEST_IMAGE_DIR = 'datasets/tti_crack/blured_image/'
-        img_size = [64, 64] #128
+        img_size = [64, 64]  # 128
 
     if 'RetinalSeg' in cfg.DATASET.TEST_IMAGE_DIR and args.origin_img_size:
         img_size = [560, 560]
